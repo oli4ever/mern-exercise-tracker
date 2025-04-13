ecd7a64c28436aeeb48fa3644d57eb52c3063254
Oli <oli.cste10@gmail.com>
2025-04-13 12:39:04 +0600

    Fix dependency installation

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+require('dotenv').config();
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
